03c9bf3e3fe8b7b7fb6a3b743f31575f81c2762f
Mradul Agrawal <mradulagrawal126@gmail.com>
2021-09-15 20:26:13 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
 
-function App() {
+let foodlist = {
+  NorthIndian: ["KadhaiPaneer", "PaneerLababdar", "MatarPaneer"],
+  SouthIndian: ["IdliSambhar", "VadaSambhar", "Dosa"],
+  Sweets: ["Rasgulla", "Rasmalai", "Gulabjamun"],
+  Namkeen: ["Sev", "Mixture", "Bundi", "Gathiya"]
+};
+
+export default function App() {
+  let itemKey = Object.keys(foodlist);
+
+  const [list1, setlist] = useState([]);
+
+  function inputClickHandler(item) {
+    let listValue = foodlist[item];
+    setlist(listValue);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Heading">
+        <h1>Food Recommendation</h1>
+        <h2>Click on any option to see some Recommendations</h2>
+      </div>
+      {itemKey.map(function (item) {
+        return (
+          <span
+            className="list-btn"
+            key={item}
+            style={{
+              padding: "1rem",
+              borderRadius: "1rem",
+              border: "solid black",
+              cursor: "pointer",
+              margin: "1rem",
+              backgroundColor: "yellow"
+            }}
+            onClick={() => inputClickHandler(item)}
+          >
+            {item}
+          </span>
+        );
+      })}
+      {list1.map(function (item) {
+        return (
+          <div
+            key={item}
+            className="list-btn"
+            style={{
+              padding: "1rem",
+              borderRadius: "1rem",
+              border: "solid black",
+              cursor: "pointer",
+              margin: "2rem",
+              backgroundColor: "yellow"
+            }}
+          >
+            {item}
+          </div>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
